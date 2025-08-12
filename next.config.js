d4e76cefe47930c49ba6fe5e7bb0d3392e5f72a7
Ignacio Aguilar <info@startwithgenie.com>
2025-08-12 19:20:12 -0400

    lol

--- next.config.js
+++ next.config.js
@@ -34,7 +34,7 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://js.stripe.com https://maps.googleapis.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:; connect-src 'self' https://api.stripe.com https://maps.googleapis.com https://api.openai.com https://xdzmaipfdwkcsxhdapla.supabase.co; frame-src 'self' https://js.stripe.com;",
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://js.stripe.com https://maps.googleapis.com https://www.googletagmanager.com https://www.clarity.ms; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:; connect-src 'self' https://api.stripe.com https://maps.googleapis.com https://api.openai.com https://xdzmaipfdwkcsxhdapla.supabase.co https://www.google-analytics.com https://analytics.google.com; frame-src 'self' https://js.stripe.com;",
           },
         ],
       },
