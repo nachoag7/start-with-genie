ffffdf78ab27d7013292346db79077112b5df88e
Ignacio Aguilar <info@startwithgenie.com>
2025-08-12 19:11:13 -0400

    yoo

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,21 +80,17 @@ export default function RootLayout({
           />
         )}
         {/* Google Analytics */}
-        {process.env.NODE_ENV === 'production' && (
-          <>
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-RVEM4FWEJ3"></script>
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', 'G-RVEM4FWEJ3');
-                `
-              }}
-            />
-          </>
-        )}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RVEM4FWEJ3"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-RVEM4FWEJ3');
+            `
+          }}
+        />
         {/* Google Ads */}
         {process.env.NODE_ENV === 'production' && (
           <>
