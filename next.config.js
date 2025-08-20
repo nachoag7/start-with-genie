78058c4e6de3f60aa62002577765631d7fd6b46c
Ignacio Aguilar <info@startwithgenie.com>
2025-08-20 01:04:26 -0400

    lol

--- next.config.js
+++ next.config.js
@@ -34,7 +34,7 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://js.stripe.com https://maps.googleapis.com https://www.googletagmanager.com https://www.clarity.ms; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:; connect-src 'self' https://api.stripe.com https://maps.googleapis.com https://api.openai.com https://xdzmaipfdwkcsxhdapla.supabase.co https://www.google-analytics.com https://analytics.google.com; frame-src 'self' https://js.stripe.com;",
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://js.stripe.com https://maps.googleapis.com https://www.googletagmanager.com https://www.clarity.ms https://www.google-analytics.com https://www.googleadservices.com https://www.pinterest.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:; connect-src 'self' https://api.stripe.com https://maps.googleapis.com https://api.openai.com https://xdzmaipfdwkcsxhdapla.supabase.co https://www.google-analytics.com https://analytics.google.com https://www.googleadservices.com https://www.google.com https://www.pinterest.com; frame-src 'self' https://js.stripe.com https://www.googletagmanager.com; worker-src 'self' blob:;",
           },
         ],
       },
